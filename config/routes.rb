296dfd2c859e39d5c47f7d6111d6b43cc2568137
Christopher McLean <havok2905@gmail.com>
2015-06-22 02:57:34 -0400

    added index page and better login/logout links

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :profiles, controller: 'profiles', only: [:index, :show, :new, :create, :update, :edit]
   devise_for :users, controllers: { omniauth_callbacks: 'callbacks' }
 
+  root 'index#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
