22baaa319f9bd727a387fe46cf11feaf7d37bb5c
Christopher McLean <havok2905@gmail.com>
2015-07-11 20:42:15 -0400

    added repo spec

--- spec/models/repo_spec.rb
+++ spec/models/repo_spec.rb
@@ -15,7 +15,15 @@ RSpec.describe Repo do
 
   context 'instance methods' do
     context '#primary_language' do
-      # TODO: add test here
+      it 'should return the primary language' do
+        repo = create :repo, whitelist: true, repo_languages: [
+          RepoLanguage.create({ name: 'javascript', bytes: 1000000 }),
+          RepoLanguage.create({ name: 'php', bytes: 100000 }),
+          RepoLanguage.create({ name: 'c', bytes: 100000 }),
+        ]
+
+        expect(repo.primary_language).to eq('javascript') 
+      end
     end
   end
 end
